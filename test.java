5b7f99b5f6dd65afbdc828b1a26c55b3e530edab
Mukund Agarwal <93988998+mukdgithub@users.noreply.github.com>
2024-02-05 20:51:11 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,17 @@
+import java.util.*;
 
+class pat1{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int i,j,n;
+        System.out.println(" enter the length ");
+        n=sc.nextInt();
+        
+        for(i=0;i<n;i++){
+            for(j=0;j<=i;j++){
+                System.out.print(" * ");    
+            }
+            System.out.println();
+        }
+    }
+}
